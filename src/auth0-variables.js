85f46489206228cbd05cc063b00985a0e588c8f4
USGill <56804741+USGill@users.noreply.github.com>
2020-06-09 23:20:36 +0530

    Fist commit

---
+++ src/auth0-variables.js
@@ -0,0 +1,6 @@
+export const AUTH_CONFIG = {
+  domain: "<dev-7wx4pvnu.auth0.com>",
+  roleUrl: "https://rbac-tutorial-app/role",
+  clientId: "<clng78N7Ekm33rzkw0u0caUVvBbZ1iuI>",
+  callbackUrl: "http://localhost:3000/callback"
+};
